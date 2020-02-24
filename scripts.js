ffcf673e63cca7dba5672feac0b68b94904aaaf9
nRudzy <omedxhd@gmail.com>
2020-02-24 11:33:26 +0100

    Responsive Website

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Fonction expression pour sélectionner des éléments
+const selectElement = (s) => document.querySelector(s);
+
+// Ouvrir le menu au clic
+selectElement('.open').addEventListener('click', () => {
+    selectElement('.nav-list').classList.add('active');
+});
+
+// Fermer le menu au clic
+selectElement('.close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
